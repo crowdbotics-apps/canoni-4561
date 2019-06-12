e2f9de274812f2e36349176bf38fbe0f51193670
Crowdbotics <team@crowdbotics.com>
2019-06-12 10:15:50 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.canoni_4561.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "heroku" {
+  version = "~> 1.5"
+}
+provider "cloudflare" {
+  version = "~> 1.9"
+}
+terraform {
+  backend "s3" {
+    bucket  = "crowdbotics-terraform"
+    key     = "canoni-4561"
+    region  = "us-east-1"
+    encrypt = "true"
+  }
+}
